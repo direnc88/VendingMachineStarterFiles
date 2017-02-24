c4096ca34375f3bc1d3d5a2aa02c6ad468aae914
direnc88 <charlesdirenzo@gmail.com>
2017-02-24 15:15:54 -0600

    Vending Machine App
    An app built with the help of Treehouse.com to learn how to use error
    handling, enumerations, and modeling an interface.

---
+++ VendingMachine/DepositController.swift
@@ -0,0 +1,28 @@
+//
+//  DepositController.swift
+//  VendingMachine
+//
+//  Created by Charles Di Renzo on 9/6/16.
+//  Copyright © 2016 Treehouse. All rights reserved.
+//
+
+import UIKit
+
+class DepositController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func dismiss() {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+}
